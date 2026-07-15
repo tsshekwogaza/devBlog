9ff7c2fe115cfd694e8964bc5820fb2dc917a6f1
Timothy Samuel <timothysamuelshekwogaza@gmail.com>
2026-07-15 23:59:12 +0100

    fix assets

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,8 +56,6 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
         ],
 
     ],
